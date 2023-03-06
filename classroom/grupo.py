77c087f5a00f19f07fa8b13e9846669b10adbc43
angomezz <angomezz@unal.edu.co>
2023-03-06 13:28:09 -0500

    :+1:

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -19,7 +19,7 @@ class Grupo:
         self.listadoAlumnos = self.listadoAlumnos + lista
 
     def __str__(self):
-        return "Grupo de estudiantes: " + self._grupo
+        return f'Grupo de estudiantes: {self._grupo}'
 
     @ classmethod
     def asignarNombre(cls, nombre="Grado 10"):
